431ea8a9c70bad4a8c80bdfae4f6da3f572c2fff
Piotr Andruchowski <andrupio@gmail.com>
2021-03-31 20:58:55 +0200

    Zad. 7.3

--- fakeaddressbook.py
+++ fakeaddressbook.py
@@ -1,19 +1,59 @@
 from faker import Faker
 fake = Faker()
-class person:
-   def __init__(self, name, address, email):
-       self.name = name
-       self.address = address
-       self.email = email
-   def __str__(self):
-       return f'{self.name} {self.address} {self.email}'
+class BaseContact:
+    def __init__(self, first_name, last_name, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+    def contact(number):
+        for i in range(number):
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            job = fake.job()
+            email = fake.email()
+            print(f"Kontaktuję się z {first_name} {last_name}, {job}, {email}")
+    #contact(3)
+    @property
+    def suma(self):
+        return len(self.first_name + ' ' + self.last_name)
 
-card = person(fake.name(), fake.address(), fake.email())
-print(card)
+    def __repr__(self):
+        #return f'{self.first_name} {self.last_name} {self.email} {self.suma}'
+         return f"{self.first_name} {self.last_name} {self.email} Suma długości imienia i nazwiska oddzielonych spacją = {self.suma}"
+class BusinessContact(BaseContact):
+   def __init__(self, job, company, phone_company, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.job = job
+       self.company = company
+       self.phone_company = phone_company
+card = BaseContact(fake.first_name(), fake.last_name(), fake.job(), fake.email())
 
-# list = []
-# for i in range (5):
-#     list.append(person(fake.name(), fake.address(), fake.email()))
+list = []
+for i in range (5):
+    list.append(BaseContact(fake.first_name(), fake.last_name(), fake.job(), fake.email()))
 
+card1 = list[0]
+card2 = list[1]
+card3 = list[2]
+card4 = list[3]
+card5 = list[4]
+cards = [card1, card2, card3, card4, card5]
+print(card1, card2, card3, card4, card5)
+
+#cards = [list]
+by_first_name = sorted(cards, key=lambda BaseContact: BaseContact.first_name)
+by_last_name = sorted(cards, key=lambda BaseContact: BaseContact.last_name)
+by_email = sorted(cards, key=lambda BaseContact: BaseContact.email)
+
+
+
+# print(by_first_name)
+# print(by_last_name)
+# print(by_email)
+
+#print(by_first_name)
+#print(cards)
 # for i in list:
-#     print(i.name,",", i.address,",", i.email)
+#     # print(by_first_name)
+#     print(i.first_name,",", i.last_name, ",", i.email)
