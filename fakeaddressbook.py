3ecd9e17769b692a29841400b984581a7cdc475f
Piotr Andruchowski <andrupio@gmail.com>
2021-04-06 22:36:52 +0200

    Zad. 7.3

--- fakeaddressbook.py
+++ fakeaddressbook.py
@@ -7,35 +7,50 @@ class BaseContact:
         self.phone = phone
         self.email = email
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.contact_phone}'
+
+    @property
+    def contact_phone(self):
+        return self.phone
+
     @property
     def label_length(self):
         return len(self.first_name + self.last_name)
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name} {self.phone} {self.email}'
-
     def contact(self):
         print(f'Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.last_name}, Label length = {self.label_length}')
-
 class BusinessContact(BaseContact):
-    def __init__(self, job, company, phone_company, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, job, company, company_phone, *args, **kwargs):
+        super().__init__( *args, **kwargs)
         self.job = job
         self.company = company
-        self.phone_company = phone_company
+        self.company_phone = company_phone
 
-    def __str__(self):
-        return f'{self.first_name} {self.last_name} {self.phone} {self.email} {self.job} {self.company} {self.phone_company} '
+    @property
+    def contact_phone(self):
+        return self.company_phone
 
 def create_contacts(type, number):
+    contacts = []
     if type == "base":
         for i in range(number):
-            card = BaseContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email())
-            print(card, card.contact())
+            priv = BaseContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email())
+            contacts.append(priv)
+            print(priv.contact(), priv)
     elif type == "business":
         for i in range(number):
-            card = BusinessContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email(), job = fake.job(), company = fake.company(), phone_company = fake.phone_number())
-            print(card, card.contact())
+            bus = BusinessContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email(), job = fake.job(), company = fake.company(), company_phone = fake.phone_number())
+            contacts.append(bus)
+            print(bus.contact(), bus)
+    return contacts
 
-create_contacts("business", 5)
+create_contacts("business", 2)
 
+
+
+
+# priv = BaseContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email())
+# buss = BusinessContact(first_name = fake.first_name(), last_name = fake.last_name(), phone = fake.phone_number(), email = fake.email(), job = fake.job(), company = fake.company(), company_phone = fake.phone_number())
+# print(priv.contact())
+# print(buss.contact())
